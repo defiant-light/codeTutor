8c9b8a047aae3c7b42e82d958c41f88725e1ec61
Robert Ing <rmi22186@gmail.com>
2015-01-30 16:02:02 -0800

    <feat> Simple user Authorization. Still need to do hashing/salting of passwords

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var crypto = require('crypto');
-var User = require('./db/index');
+var User = require('./server/user/userController');
 // config file to instantiate all queues
 var queues = require('./server/queue/queueCollection.js');
 var queueModel = require('./server/queue/queueModel.js');
@@ -17,6 +17,10 @@ console.log('Server now listening on port ' + port);
 
 // app.use(partials());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.set('views', __dirname);
 app.use(express.static(__dirname));
 console.log(__dirname);
@@ -58,42 +62,8 @@ app.get('/api/getroom', function(request, response) {
   }
 });
 
-app.get('/signup', function(request, response) {
-  response.redirect('./client/signup.html');
-});
+app.get('/signup', User.signUpUserForm);
+app.post('/signup', User.signUpUser);
 
-app.post('/signup', function(req, res) {
-  console.log('post request confirmed');
-  // var username=req.body.username;
-  console.log(req.body);
-  // var password=req.body.password;
-
-  // User.findOne({ where : { 
-  //   username: username }
-  // })
-  //   .complete(function(err, user) {
-  //     console.log(user);
-  //     if (user) {
-  //       // User.create()
-  //       console.log("pick a new username");
-  //     }
-  //     if (err) {
-  //       User
-  //         .create({
-  //           username: username,
-  //           firstname: req.body.firstname,
-  //           lastname: req.body.lastname,
-  //           password: password,
-  //           // desired: 'english'
-  //         })
-  //         .complete(function(err, user) {
-  //           if (!!err) {
-  //             console.log('An error occurred while creating the table: user.create', err);
-  //           } else {
-  //             console.log('User created: ', user);
-  //             res.render('/index.html');
-  //           }
-  //         });
-  //     }
-  //   });
-});
+app.get('/signin', User.signInUserForm);
+app.post('/signin', User.signInUser);
--- server/user/userController.js
+++ server/user/userController.js
@@ -1,14 +1,13 @@
 var User = require('../../db/index');
-var bodyParser = require('body-parser');
-// var request = require('request');
+// var bodyParser = require('body-parser');
 
-// exports.signupUserForm = function(req, res) {
-//   res.render('signup');
-// };
+exports.signUpUserForm = function(req, res) {
+  res.redirect('../../client/signup.html');
+};
 
-// exports.loginUserForm = function(req, res) {
-//   res.render('login');
-// };
+exports.signInUserForm = function(req, res) {
+  res.redirect('../../client/signin.html');
+};
 
 // exports.logoutUser = function(req, res) {
 //   // req.session.destroy(function(){
@@ -16,71 +15,88 @@ var bodyParser = require('body-parser');
 //   // });
 // };
 
-// exports.login = function(req, res) {
-//   var username = req.body.username;
-//   var password = req.body.password;
+exports.signInUser = function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
 
-//   User.findOne({ username:username })
-//     .then(function(user){
-//         bcrypt.compare(password, user.attributes.password, function(err, result) {
-//           if (result) {
-//             req.session.regenerate(function(){
-//               req.session.user = username;
-//               res.redirect('/');
-//             });
-//           } else {
-//             console.log('wrooooong password or log in!');
-//             res.render('login');
-//           }
-//         });
-//       })
-//     .catch(function() {
-//       res.render('login');
-//     });
+  User
+    .findOne({ where: 
+      { username: username }
+    })
+    .then(function(user){
+      // console.log(user.username);
+        if (user.password === password) {
+          res.redirect('../../index.html');
+        } else {
+          res.redirect('../../client/signin.html');
+        }
+      })
+    .catch(function() { 
+      res.redirect('../../client/signin.html');
+    });
+};
+        // bcrypt.compare(password, user.attributes.password, function(err, result) {
+          // if (result) {
+            // req.session.regenerate(function(){
+              // req.session.user = username;
+              // res.redirect('/');
+            // });
+          // } else {
+            // console.log('wrooooong password or log in!');
+            // res.render('login');
+          // }
+        // });
+      // })
+    // .catch(function() {
+      // res.render('login');
 
-//   // User.find({ username: username })
-//   //   .then(function(user) {
-//   //     if (!user) {
-//   //       res.redirect('/login');
-//   //     } else {
-//   //       user.comparePassword(password, function(match) {
-//   //         if (match) {
-//   //           util.createSession(req, res, user);
-//   //         } else {
-//   //           res.redirect('/login');
-//   //         }
-//   //       });
-//   //     }
-//   // });
-// };
 
-exports.signupUser = function(req, res) {
+  // User.find({ username: username })
+  //   .then(function(user) {
+  //     if (!user) {
+  //       res.redirect('/login');
+  //     } else {
+  //       user.comparePassword(password, function(match) {
+  //         if (match) {
+  //           util.createSession(req, res, user);
+  //         } else {
+  //           res.redirect('/login');
+  //         }
+  //       });
+  //     }
+  // });
+
+
+//TO DO: salt and hash passwords
+exports.signUpUser = function(req, res) {
   var username=req.body.username;
   var password=req.body.password;
 
-  User.find({ username: username })
-    .complete(function(err, user) {
-      if (user) {
-        // User.create()
-        console.log("pick a new username");
-      }
-      if (err) {
-        User
-          .create({
-            username: username,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            password: password,
-            // desired: 'english'
-          })
-          .complete(function(err, user) {
-            if (!!err) {
-              console.log('An error occurred while creating the table: user.create', err);
-            } else {
-              console.log('User created: ', user);
-              res.render('/index.html');
-            }
-          });
-      }
-    });
-  };
+  User.findOne({ where : { 
+    username: username }
+  })
+  .complete(function(err, user) {
+    if (user) {
+      console.log("pick a new username");
+    }
+    if (!user) {
+      User
+        .create({
+          username: username,
+          password: password, //salt and hash password
+          // firstname: req.body.firstname, //add later?
+          // lastname: req.body.lastname, //add later?
+          // native: req.body.native, //add later?
+          // desired: req.body.desired //add later?
+        })
+        .complete(function(err, user) {
+          if (!!err) {
+            console.log('An error occurred while creating the table: user.create', err);
+          } else {
+            console.log('User created: ', user.username);
+            res.redirect('./index.html');
+          }
+        });
+    }
+  });
+};
