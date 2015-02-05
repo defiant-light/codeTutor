3f719177c9ffdf094636fa39334b5b6558ce2341
Luke Davis <lukefable@gmail.com>
2015-02-05 00:06:36 +0000

    removed the signup page.

--- client/mainApp.js
+++ client/mainApp.js
@@ -5,21 +5,17 @@ angular.module('mainApp',["ui.router"
     $urlRouterProvider.otherwise("/signin");
 
     $stateProvider
-	    .state('signup', {
-        url:'/signup',
-        templateUrl: 'client/signup.html'
-      })
       .state('signin', {
         url:'/signin', 
         templateUrl: 'client/signin.html'
       })
-			.state('videochat', {
-        url:'/videochat',
-        templateUrl: 'client/videoChat.html'
-      })
       .state('selectsubject', {
         url:'/selectsubject', 
         templateUrl: 'client/selectSubject.html'
+      })
+			.state('videochat', {
+        url:'/videochat',
+        templateUrl: 'client/videoChat.html'
       })
 			.state('ratepartner', {
         url:'/ratepartner',
