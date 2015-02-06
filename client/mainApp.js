35b418613b2dd58baf3675731c9f85bf0d828849
Todd Skinner <todd.skinner@gmail.com>
2015-02-05 16:47:58 -0800

    fix merge crap

---
+++ client/mainApp.js
@@ -0,0 +1,34 @@
+angular.module('mainApp',["ui.router","selectsubject","videochat", "ratepartner"
+	])
+	.config(function($stateProvider, $urlRouterProvider){
+		
+    $urlRouterProvider.otherwise("/signin");
+
+    $stateProvider
+      .state('signin', {
+        url:'/signin', 
+        templateUrl: 'client/signin.html'
+      })
+      .state('selectsubject', {
+        url:'/selectsubject', 
+        templateUrl: 'client/selectSubject.html'
+      })
+			.state('videochat', {
+        url:'/videochat',
+        templateUrl: 'client/videoChat.html'
+      })
+			.state('ratepartner', {
+        url:'/ratepartner',
+        templateUrl: 'client/ratepartner.html'
+      })
+
+      // .state('logout', {
+      //   url: '/signin', 
+      //   templateUrl: 'client/signin.html'
+      // })
+      
+	});
+	})
+.factory('video', function () {
+  return {options:null};
+});
